75066c141e66fa57fbf01a6151dad7b52e46a5ad
Fernando Ferreira <nandobritoferreira@hotmail.com>
2025-07-11 19:54:05 -0300

    fix(front - buscar-jogos): alterar da pasta de components para pages

---
+++ FrontEnd/frontend/src/components/BuscarJogos.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import ListaJogos from "./ListaJogos";
+
+export default function BuscarJogos() {
+  const [nomeBuscado, setNomeBuscado] = useState('');
+  const [resultados, setResultados] = useState([]);
+
+  async function BuscarJogoPorNome(nomeBuscado) {
+    console.log(nomeBuscado); // mostra o que o usuário digita
+    const url = `https://api.rawg.io/api/games?search=${nomeBuscado}&key=${import.meta.env.VITE_RAWG_API_KEY}`;
+    console.log(url);
+
+    const resposta = await fetch(url);
+    const data = await resposta.json();
+    console.log(data.results);
+    setResultados(data.results);
+  }
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setNomeBuscado(e.target.value)}
+      />
+      <button
+        onClick={() => BuscarJogoPorNome(nomeBuscado)}
+      >Buscar</button>
+
+      <div>
+        <ListaJogos jogos={resultados} />
+      </div>
+    </div>
+  );
+}
+
+
